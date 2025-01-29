6f773455d093298b67be032f4700b1963541bc8b
Marco <mrodasw@gmail.com>
2025-01-29 08:56:43 -0600

    feat: added local search and persist data on local storage

--- src/components/pages/tasks/task-section.tsx
+++ src/components/pages/tasks/task-section.tsx
@@ -1,19 +1,50 @@
 "use client";
 
+import { Input } from "@/components/ui/input";
 import { columns } from "@/lib/shared/mocks/colums.mock";
 import { useTaskStore } from "@/lib/store/task-store";
-import { TaskStatus } from "@/lib/types/task.types";
-import { DndContext, type DragEndEvent } from "@dnd-kit/core";
-import { useEffect } from "react";
-import { toast } from "sonner";
+import { type Task, TaskStatus } from "@/lib/types/task.types";
+import {
+	DndContext,
+	type DragEndEvent,
+	PointerSensor,
+	TouchSensor,
+	useSensor,
+	useSensors,
+} from "@dnd-kit/core";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import TaskColum from "./task-colum";
 export default function TaskSection() {
-	const { tasks, getTasks, updateTask } = useTaskStore();
+	const tasks = useTaskStore((state) => state.tasks);
+	const getTasks = useTaskStore((state) => state.getTasks);
+	const updateTask = useTaskStore((state) => state.updateTask);
+
+	const sensors = useSensors(useSensor(PointerSensor), useSensor(TouchSensor));
+
+	const [filteredTasks, setFilteredTasks] = useState<Task[]>(tasks);
+
+	const router = useRouter();
+	const searchParams = useSearchParams();
+	const searchQuery = searchParams.get("search") || "";
 
 	useEffect(() => {
 		// Fetch tasks on component mount
 		getTasks();
 	}, [getTasks]);
+
+	useEffect(() => {
+		const filtered = tasks.filter(
+			(task) =>
+				task.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+				task.description.toLowerCase().includes(searchQuery.toLowerCase()),
+		);
+		setFilteredTasks(filtered);
+	}, [tasks, searchQuery]);
+
+	const handleSearch = (query: string) => {
+		router.push(`?search=${encodeURIComponent(query)}`, { scroll: false });
+	};
 
 	const handleDragEnd = async (event: DragEndEvent) => {
 		const { active, over } = event;
@@ -26,25 +57,33 @@ export default function TaskSection() {
 					status: TaskStatus.Completed,
 					completedAt: new Date(),
 				});
-				toast.success("Tarea actualizada");
 				return;
 			}
 			await updateTask({ ...item, status: over.id as TaskStatus });
-			toast.success("Tarea actualizada");
 		}
 	};
 
 	return (
-		<section className="mt-24 h-full w-full flex flex-row gap-2 items-start justify-evenly">
-			<DndContext onDragEnd={handleDragEnd}>
-				{columns.map((value, index) => (
-					<TaskColum
-						key={value.id}
-						column={value}
-						tasks={tasks.filter((t) => t.status === value.id)}
-					/>
-				))}
-			</DndContext>
+		<section className="mt-24 h-full w-full flex flex-col gap-4">
+			<div className="flex flex-row items-center justify-between">
+				<Input
+					className="max-w-md"
+					placeholder="Buscar tareas..."
+					value={searchQuery}
+					onChange={(e) => handleSearch(e.target.value)}
+				/>
+			</div>
+			<div className="flex flex-row gap-2 items-start justify-evenly">
+				<DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+					{columns.map((value, index) => (
+						<TaskColum
+							key={value.id}
+							column={value}
+							tasks={filteredTasks.filter((t) => t.status === value.id)}
+						/>
+					))}
+				</DndContext>
+			</div>
 		</section>
 	);
 }
--- src/lib/store/task-store.ts
+++ src/lib/store/task-store.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { initialTasks } from "../shared/mocks/tasks.mock";
+import { persist } from "zustand/middleware";
 import type { Task } from "../types/task.types";
 
 interface TaskStore {
@@ -10,44 +10,52 @@ interface TaskStore {
 	deleteTask: (taskId: string) => Promise<void>;
 }
 
-export const useTaskStore = create<TaskStore>((set, get) => ({
-	tasks: initialTasks,
+export const useTaskStore = create<TaskStore>()(
+	persist(
+		(set, get) => ({
+			tasks: [],
 
-	// Fetch tasks
-	getTasks: async () => {
-		const tasks = await Promise.resolve(initialTasks);
-		set({ tasks });
-	},
+			// Fetch tasks
+			getTasks: async () => {
+				const tasks = get().tasks;
+				if (tasks.length > 0) return;
+				set({ tasks });
+			},
 
-	// Create a new task
-	createTask: async (task) => {
-		const randomId = Math.floor(Math.random() * 1000000);
-		const newTask: Task = {
-			...task,
-			id: randomId.toString(),
-			completedAt: null,
-		};
-		const updatedTasks = [...get().tasks, newTask];
-		set({ tasks: updatedTasks });
-		return Promise.resolve();
-	},
+			// Create a new task
+			createTask: async (task) => {
+				const randomId = Math.floor(Math.random() * 1000000);
+				const newTask: Task = {
+					...task,
+					id: randomId.toString(),
+					completedAt: null,
+				};
+				const updatedTasks = [...get().tasks, newTask];
+				set({ tasks: updatedTasks });
+				return Promise.resolve();
+			},
 
-	// Update an existing task
-	updateTask: async (task) => {
-		const tasks = get().tasks;
-		const index = tasks.findIndex((t) => t.id === task.id);
-		if (index === -1) {
-			throw new Error("Task not found");
-		}
-		tasks[index] = task;
-		set({ tasks: [...tasks] });
-		return Promise.resolve();
-	},
+			// Update an existing task
+			updateTask: async (task) => {
+				const tasks = get().tasks;
+				const index = tasks.findIndex((t) => t.id === task.id);
+				if (index === -1) {
+					throw new Error("Task not found");
+				}
+				tasks[index] = task;
+				set({ tasks: [...tasks] });
+				return Promise.resolve();
+			},
 
-	// Delete a task
-	deleteTask: async (taskId) => {
-		const tasks = get().tasks.filter((t) => t.id !== taskId);
-		set({ tasks });
-		return Promise.resolve();
-	},
-}));
+			// Delete a task
+			deleteTask: async (taskId) => {
+				const tasks = get().tasks.filter((t) => t.id !== taskId);
+				set({ tasks });
+				return Promise.resolve();
+			},
+		}),
+		{
+			name: "task-storage",
+		},
+	),
+);
